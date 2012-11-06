eb563e31e498a4b0521bfb8c6c8ee3a9bc368373
yassine <yassine@yassine-1025C.(none)>
2012-11-06 18:40:31 +0100

    ajout scrollbar

--- src/com/example/whowants/view/PlayActivity.java
+++ src/com/example/whowants/view/PlayActivity.java
@@ -14,7 +14,6 @@ import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -23,17 +22,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.whowants.R;
-import com.example.whowants.R.menu;
 import com.example.whowants.model.Game;
 import com.example.whowants.model.Question;
 
